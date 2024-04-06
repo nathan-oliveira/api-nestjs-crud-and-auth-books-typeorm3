ac412a8fa95e60fb890195167587df72caa5a403
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2024-04-06 10:44:19 -0300

    feat: decorator user auth

--- src/common/decorators/user-auth.decorator.ts
+++ src/common/decorators/user-auth.decorator.ts
@@ -1,8 +1,11 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 import { LoginUserDto } from 'src/modules/auth/dtos';
 
-export const UserAuth = createParamDecorator((data: unknown, req: Request) => {
-  return <LoginUserDto>req.user;
-});
+export const UserAuth = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest<Request>();
+    return <LoginUserDto>req.user;
+  },
+);
